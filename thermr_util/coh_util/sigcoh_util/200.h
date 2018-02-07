d31bd7eb4e32e034c2ddcd1b4ccbe941161593b0
Amelia Trainer <ameliajo@mit.edu>
2018-02-07 16:59:56 -0500

    added two tests for the 200 read braggs parameters

--- thermr_util/coh_util/sigcoh_util/200.h
+++ thermr_util/coh_util/sigcoh_util/200.h
@@ -2,21 +2,15 @@
 #include <vector>
 
 
-auto readBraggParameters( std::vector<double> fl, int l, double econ ){
-  int k;
-  
-
- // bragg parameters already read from endf6
-   k=fl[6];
-   int nl=k;
-   double blast=0;
-   double scon=1;
-   double lenext=fl[9];
-   //enext=sigfig(enext,7,-1);
-   for ( int i = 1; i < nl; ++i ){
-      l=1+2*(i-1);
-      fl[l]=fl[l+8]*econ;
-      fl[l+1]=fl[l+9]-blast;
-      blast=fl[l+9];
-    } 
+auto readBraggParameters( std::vector<double>& fl, int l, double econ ){
+  // bragg parameters already read from endf6
+  int nl = fl[5];
+  double blast = 0, scon = 1, lenext = fl[9];
+  //enext=sigfig(enext,7,-1);
+  for ( int i = 1; i <= nl; ++i ){
+    l = 1 + 2 * (i-1);
+    fl[l-1] = fl[l+8-1] * econ;
+    fl[l+1-1] = fl[l+9-1] - blast;
+    blast = fl[l+9-1];
+  } 
 }
--- thermr_util/coh_util/sigcoh_util/200.test.cpp
+++ thermr_util/coh_util/sigcoh_util/200.test.cpp
@@ -5,9 +5,33 @@
 
 TEST_CASE( "read bragg parameters" ){
   GIVEN( "" ){
-    std::vector<double> fl 
+    std::vector<double> fl { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0,
+    11.11, 12.12, 13.13, 14.14, 15.15, 16.16, 0, 0, 0, 0, 0, 0 };
+    int l = 1;
+    double econ = 1.93038465E+19;
+    readBraggParameters( fl, l, econ );
+    std::vector<double> correctFl { 1.9110807E20, 10, 2.1446572E20, 2.12, 
+      2.5345950E20, 2.02, 2.924532E20, 2.02, 0, -16.16, 0, 0, 13.13, 14.14, 
+      15.15, 16.16 };
+    for ( size_t i = 0; i < correctFl.size(); ++i ){ 
+      REQUIRE( correctFl[i] == Approx( fl[i] ).epsilon(1e-6) ); 
+    }
     THEN( "" ){
       REQUIRE( true );
     } // THEN
+  } // GIVEN
+  GIVEN( "" ){
+    std::vector<double> fl { 11.1, 12.2, 13.3, 14.4, 15.5, 16.6, 17.7, 18.8, 
+      19.9, 120.0, 121.11, 122.12, 123.13, 124.14, 125.15, 126.16, 0, 0, 0, 0, 
+      0, 0, 0, 0, 0, 0, 0 };
+    int l = 1;
+    double econ = 1.93038465E+19;
+    readBraggParameters( fl, l, econ );
+    std::vector<double> correctFl { 3.841465E20, 120, 2.337888E21, 2.12, 
+      2.3768825E21, 2.02, 2.4158764E21, 2.02, 0, -126.16, 0, 0, 0, 0, 0, 0 };
+    for ( size_t i = 0; i < correctFl.size(); ++i ){ 
+      REQUIRE( correctFl[i] == Approx( fl[i] ).epsilon(1e-6) ); 
+    }
+
   } // GIVEN
 } // TEST CASE
