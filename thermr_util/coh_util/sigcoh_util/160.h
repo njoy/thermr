2e365642dc6d38aacb2881053836da0a77e7eaec
Amelia Trainer <ameliajo@mit.edu>
2018-02-07 21:03:23 -0500

    added tausq tests for 160 piece

--- thermr_util/coh_util/sigcoh_util/160.h
+++ thermr_util/coh_util/sigcoh_util/160.h
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "form.h"
 
 auto tausq( int m1, int m2, int m3, double c1, double c2 ){
   double twopis = 4 * M_PI * M_PI;
@@ -8,7 +9,9 @@ auto tausq( int m1, int m2, int m3, double c1, double c2 ){
 }
 
 auto do160( int lat, double w1, double w2, double w3, int l1, int l2, int l3,
-  double tsq, int k, double f ){
+  int k, double c1, double c2, double t2, 
+  std::vector<double>& wrk, double wint, int nw ){
+   double tsq, tau, w, f;
    tsq = tausq( l1, -l2, l3, c1, c2 );
    // if (tsq <= 0 or tsq > ulim ) go to 175
    tau = sqrt(tsq);
@@ -16,7 +19,7 @@ auto do160( int lat, double w1, double w2, double w3, int l1, int l2, int l3,
    f = w * form(lat,l1,-l2,l3);
    // if (k > 0 and tsq > tsqx) go to 165
    k = k + 1;
-   if ((2*k).gt.nw) std::cout << "oh no, sigcoh! Storage exceeded" << std::endl; } 
+   if ((2*k) > nw) { std::cout << "oh no, sigcoh! Storage exceeded" << std::endl; } 
    wrk[2*k-1] = tsq;
    wrk[2*k] = f;
 
--- thermr_util/coh_util/sigcoh_util/160.test.cpp
+++ thermr_util/coh_util/sigcoh_util/160.test.cpp
@@ -3,12 +3,28 @@
 #include "160.h"
 
 
+
+TEST_CASE( "tausq" ){
+  GIVEN( "inputs" ){
+    std::vector<std::tuple<int,int,int,double,double>> inputs
+      { {0,0,0,2,4}, {1,0,0,2,4}, {0,1,0,2,4}, {0,0,1,2,4}, {1,1,0,2,4}, 
+	{1,0,1,2,4}, {0,1,1,2,4}, {1,1,1,2,4}, {1,2,3,4,5}, {5,3,6,4,5},
+        {8,7,9,.1,.2} };
+    std::vector<double> output { 0, 78.956835, 78.956835, 157.91367041, 
+	236.870505, 236.870505, 236.870505, 394.78417604, 2881.924485, 
+	14843.885019, 1306.735642 };
+    
+    for ( auto i = 0; i < output.size(); ++i ){
+      REQUIRE( tausq( std::get<0>(inputs[i]), std::get<1>(inputs[i]),
+        std::get<2>(inputs[i]), std::get<3>(inputs[i]), 
+        std::get<4>(inputs[i]) ) == Approx(output[i]).epsilon(1e-6) );
+    }
+  } // GIVEN
+} // TEST CASE
+
+
 TEST_CASE( "computeCrossSections" ){
   GIVEN( "" ){
-    std::vector<double> s(10, 0), fl { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1 };
-    double e = 0.015;
-    int nl = 10, k = 10, nw = 10;
-    double emax = 0.625, scon = 0.2, recon = 0.5;
       
     THEN( "" ){
       REQUIRE( true );
