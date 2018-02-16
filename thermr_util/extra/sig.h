1a1d7c77feea19007d5a2554c9c3503ad2954f5c
Amelia Trainer <ameliajo@mit.edu>
2018-02-16 12:46:56 -0500

    reformatting sig

--- thermr_util/extra/sig.h
+++ thermr_util/extra/sig.h
@@ -3,27 +3,27 @@
 #include "terpq.h"
 
 auto do170( int lat, double a, double b, double tevz, double tev, 
-    double rtev, double teff, double teff2, double az, double az2, 
+    double teff, double teff2, double az, double az2, 
     double sigc, double sb, double sb2, double arg, double e, 
     double tfff, double tfff2, double sigmin, double u, 
     double c, double bb, int sabflg ){
    // short collision time for large beta or alpha
    // 170 continue
    double sigVal;
-   if (lat == 1) b = b * tevz * rtev;
-   if (lat == 1) a = a * tevz * rtev;
+   if (lat == 1) b = b * tevz / tev;
+   if (lat == 1) a = a * tevz / tev;
    tfff = teff;
    tfff2 = teff2;
 
    double s = 0;
    arg = (a-b)*(a-b)*tev/(4*a*tfff)+(b+bb)/2;
-   if (-arg > sabflg) s = exp(-arg) / (c*sqrt(a*tfff*rtev));
+   if (-arg > sabflg) s = exp(-arg) / (c*sqrt(a*tfff/tev));
    sigVal = sigc * sb * s;
    if (sb2 > 0) {
       a = a * az / az2;
       arg = (a-b)*(a-b)*tev/(4*a*tfff2)+(b+bb)/2;
       double s2 = 0;
-      if (-arg > sabflg) s2 = exp(-arg)/(c*sqrt(a*tfff2*rtev));
+      if (-arg > sabflg) s2 = exp(-arg)/(c*sqrt(a*tfff2/tev));
       sigVal = sigVal + sigc * sb2 * s2;
     }
     if (std::abs(e-10) < .01 and std::abs(u-.99219) < .0001) {
@@ -45,13 +45,16 @@ auto do155( int ia, int ib, int nalpha, int nbeta, double s, double bb,
 
   double s1, s2, s3;
   s1 = terpq( alpha[ia], alpha[ia+1], alpha[ia+2], a, sab[ia][ib], sab[ia+1][ib], sab[ia+2][ib] );
-  //std::cout << s1 << std::endl;
+  std::cout << " ------------------- " << std::endl;
+  std::cout << s1 << std::endl;
   s2 = terpq( alpha[ia], alpha[ia+1], alpha[ia+2], a, sab[ia][ib+1], sab[ia+1][ib+1], sab[ia+2][ib+1] );
-  //std::cout << s2 << std::endl;
+  std::cout << s2 << std::endl;
   s3 = terpq( alpha[ia], alpha[ia+1], alpha[ia+2], a, sab[ia][ib+2], sab[ia+1][ib+2], sab[ia+2][ib+2] );
-  //std::cout << s3 << std::endl;
+  std::cout << s3 << std::endl;
   s = terpq( beta[ib], beta[ib+1], beta[ib+2], bbb, s1, s2, s3 );
-  //std::cout << s << std::endl;
+  std::cout << s << std::endl;
+  std::cout << bb << std::endl;
+  std::cout << " ------------------- " << std::endl;
 
   std::cout << "160" << std::endl;
   double sigVal = 0;
@@ -70,44 +73,25 @@ auto do155( int ia, int ib, int nalpha, int nbeta, double s, double bb,
 
 auto do150( double a, double az, double test2, double b, int ia, int ib,
   int sabflg, int lat, double tevz, double tev, 
-  std::vector<std::vector<double>> sab, double rtev, double teff, double teff2,
+  std::vector<std::vector<double>> sab, double teff, double teff2,
   double az2, double sigc, double sb, double sb2, double e, double tfff, 
-  double tfff2, double arg, double sigmin, double u, double c, int bb, 
+  double tfff2, double arg, double sigmin, double u, double c, double bb, 
   int nalpha, int nbeta, double s, std::vector<double> alpha, 
   std::vector<double> beta, double bbb ){  //150 continue
-  //if (a*az < test2 and b < test2) go to 155
+
   if (a*az < test2 and b < test2) { 
     std::cout << "155" << std::endl; 
-    
-    return do155( ia, ib, nalpha, nbeta, s, bb, sigc, sb, sigmin, sabflg, alpha, beta, sab, a, bbb );
- 
+    return do155( ia, ib, nalpha, nbeta, s, bb, sigc, sb, sigmin, sabflg, 
+        alpha, beta, sab, a, bbb );
   } // go to 155
 
-  //if (sab(ia,ib) <= sabflg) go to 170
-  if (sab[ia][ib] <= sabflg){ // go to 170
-    return do170( lat, a, b, tevz, tev, rtev, teff, teff2, az, az2, sigc, 
+  if ( sab[ia][ib]   <= sabflg or sab[ia+1][ib]   <= sabflg or 
+       sab[ia][ib+1] <= sabflg or sab[ia+1][ib+1] <= sabflg ){
+    return do170( lat, a, b, tevz, tev, teff, teff2, az, az2, sigc, 
        sb, sb2, arg, e, tfff, tfff2, sigmin, u, c, bb, sabflg );
   }
 
-  //if (sab(ia+1,ib) <= sabflg) go to 170
-  if (sab[ia+1][ib] <= sabflg){// go to 170
-    return do170( lat, a, b, tevz, tev, rtev, teff, teff2, az, az2, sigc, 
-       sb, sb2, arg, e, tfff, tfff2, sigmin, u, c, bb, sabflg );
-  }
-
-  //if (sab(ia,ib+1) <= sabflg) go to 170
-  if (sab[ia][ib+1] <= sabflg){// go to 170
-    return do170( lat, a, b, tevz, tev, rtev, teff, teff2, az, az2, sigc, 
-       sb, sb2, arg, e, tfff, tfff2, sigmin, u, c, bb, sabflg );
-  }
-
-  //if (sab(ia+1,ib+1) <= sabflg) go to 170
-  if (sab[ia+1][ib+1] <= sabflg){// go to 170
-    return do170( lat, a, b, tevz, tev, rtev, teff, teff2, az, az2, sigc, 
-       sb, sb2, arg, e, tfff, tfff2, sigmin, u, c, bb, sabflg );
-  }
   return 0.0;
-
 }
 
 
@@ -123,19 +107,15 @@ auto sig( double e, double ep, double u, double tev, double tevz,
    * data or an analytic law.
    *-------------------------------------------------------------------
    */
-   int nb1, na1,i,ib,ia;
-   double rtev,bb,a,sigc,b,c,bbb,s,s1,s2,s3,arg,tfff,tfff2,rat,bbm;
-   double sigmin=1.e-10, sabflg=-225.e0,amin=1.e-6,test1=0.2e0,test2=30.e0;
-   double sigVal;
-
-   int nbeta = beta.size(), nalpha = alpha.size();
+   int nb1, na1,i,ib,ia,nbeta = beta.size(), nalpha = alpha.size();
+   double bb,a,sigc,b,c,bbb,s,s1,s2,s3,arg,tfff,tfff2,rat,bbm,sigmin=1.e-10, 
+          sabflg=-225.e0,amin=1.e-6,test1=0.2e0,test2=30.e0,sigVal;
 
    // common factors.
-   rtev = 1 / tev;
-   bb = (ep-e) * rtev;
+   bb = (ep-e) / tev;
    a = (e+ep-2*u*sqrt(e*ep)) / (az*tev);
    if (a < amin) a = amin;
-   sigc = sqrt(ep/e) * rtev / 2;
+   sigc = sqrt(ep/e) / tev / 2;
    b = std::abs(bb);
    c = sqrt(4*M_PI);
 
@@ -164,7 +144,7 @@ auto sig( double e, double ep, double u, double tev, double tevz,
    if (lat == 1) a = a * tev / tevz;
    if (a > alpha[nalpha-1]) std::cout << "go to 170 A" << std::endl;
    if (a > alpha[nalpha-1]) { 
-     return do170( lat, a, b, tevz, tev, rtev, teff, teff2, az, az2, sigc, 
+     return do170( lat, a, b, tevz, tev, teff, teff2, az, az2, sigc, 
        sb, sb2, arg, e, tfff, tfff2, sigmin, u, c, bb, sabflg );
    }
 
@@ -173,19 +153,19 @@ auto sig( double e, double ep, double u, double tev, double tevz,
       if (lat == 1) bbm = bb * tev / tevz;
       if (bbm > beta[nbeta-1]) { std::cout << "go to 170 B" << std::endl; }
       if (bbm > beta[nbeta-1]) {
-        return do170( lat, a, b, tevz, tev, rtev, teff, teff2, az, az2, sigc, 
+        return do170( lat, a, b, tevz, tev, teff, teff2, az, az2, sigc, 
           sb, sb2, arg, e, tfff, tfff2, sigmin, u, c, bb, sabflg );
       }
       if (bbm < beta[0]) { std::cout << "go to 170 C" << std::endl; }
       if (bbm < beta[0]) {
-        return do170( lat, a, b, tevz, tev, rtev, teff, teff2, az, az2, sigc, 
+        return do170( lat, a, b, tevz, tev, teff, teff2, az, az2, sigc, 
           sb, sb2, arg, e, tfff, tfff2, sigmin, u, c, bb, sabflg );
       }
    }
    else {
      if (b > beta[nbeta-1]) { std::cout << "go to 170 D" << std::endl; }
      if (b > beta[nbeta-1]) {
-        return do170( lat, a, b, tevz, tev, rtev, teff, teff2, az, az2, sigc, 
+        return do170( lat, a, b, tevz, tev, teff, teff2, az, az2, sigc, 
           sb, sb2, arg, e, tfff, tfff2, sigmin, u, c, bb, sabflg );
      }
    } 
@@ -196,42 +176,52 @@ auto sig( double e, double ep, double u, double tev, double tevz,
    for ( size_t i = 0; i < nb1; ++i ){
       ib = i;
       if (bbb < beta[i+1]) break;
-      //if (bbb < beta[i+1]) return sigVal;
    }
    for ( size_t i = 0; i < na1; ++i ) {
      ia = i;
      if ( a < alpha[i+1] ){ break; }
-     //if ( a < alpha[i+1] ){ return sigVal; }
    }
 
    if (cliq == 0 or a >= alpha[0]) {
      std::cout << "150 A" << std::endl;
 
-     return do150( a, az, test2, b, ia, ib, sabflg, lat, tevz, tev, sab, rtev, 
+     std::cout << " bb:   " << bb << std::endl;
+     return do150( a, az, test2, b, ia, ib, sabflg, lat, tevz, tev, sab, 
        teff, teff2, az2, sigc, sb, sb2, e, tfff, tfff2, arg, sigmin, u, c, bb, 
        nalpha, nbeta, s, alpha, beta, bbb );
 
    }
 
-   if ( lasym == 1 ) std::cout << "150 B" << std::endl;
+   if ( lasym == 1 ) {
+     std::cout << "150 B" << std::endl;
+     return do150( a, az, test2, b, ia, ib, sabflg, lat, tevz, tev, sab, 
+       teff, teff2, az2, sigc, sb, sb2, e, tfff, tfff2, arg, sigmin, u, c, bb, 
+       nalpha, nbeta, s, alpha, beta, bbb );
+    }
 
-   if (b > test1) std::cout << "150 C" << std::endl;
+
+
+   if (b > test1) {
+     std::cout << "150 C" << std::endl;
+     return do150( a, az, test2, b, ia, ib, sabflg, lat, tevz, tev, sab, 
+       teff, teff2, az2, sigc, sb, sb2, e, tfff, tfff2, arg, sigmin, u, c, bb, 
+       nalpha, nbeta, s, alpha, beta, bbb );
+    }
+
+
 
    s = sab[0][0] + log(alpha[0]/a)/2 - cliq*b*b/a;
 
    if (s < sabflg) s = sabflg;
 
    // go to 160
-  
-  //150 continue
-   //if (a*az < test2 and b < test2) go to 155
-   //if (sab(ia,ib) <= sabflg) go to 170
-   //if (sab(ia+1,ib) <= sabflg) go to 170
-   //if (sab(ia,ib+1) <= sabflg) go to 170
-   //if (sab(ia+1,ib+1) <= sabflg) go to 170
-   
+   std::cout << "160" << std::endl;
+  sigVal = 0;
+  if (s-bb/2 > sabflg) sigVal = exp(s-bb/2);
+  sigVal = sigc * sb * sigVal;
+  if (sigVal < sigmin) sigVal = 0;
+  return sigVal;
 
-   // 155
 
    // free-gas scattering.
   // 200 continue
--- thermr_util/extra/sig.test.cpp
+++ thermr_util/extra/sig.test.cpp
@@ -4,7 +4,6 @@
 
 
 TEST_CASE( "compute differential scattering ccross sections" ){
-  GIVEN( "" ){
     double e = 1.5, ep = 0, u = -1, tev =  3.5E-2, tevz = 2.53E-2, 
       az = 11.898, az2 = 0, sigVal;
     std::vector<double> alpha { 2.25, 2.30, 2.35, 2.40 }, beta { 0.02, 0.2, 1.2 };
@@ -12,7 +11,7 @@ TEST_CASE( "compute differential scattering ccross sections" ){
     double cliq = 0, sb = 0, sb2 = 0, teff = 0, teff2 = 0;
     std::vector<std::vector<double>> sab { {0.1, 0.2, 0.3}, {1.4, 1.5, 1.6}, {2.1, 2.2, 2.3}, {3.4, 3.5, 3.6} };
 
-/*
+  GIVEN( "lat = 0" ){
 
     WHEN( "temperature is high" ){
       AND_WHEN( "energy e is relatively low" ){
@@ -43,9 +42,63 @@ TEST_CASE( "compute differential scattering ccross sections" ){
           sigVal = sig( e, ep, u, tev, tevz, alpha, beta, sab, az, az2, lat, iinc, lasym, cliq, sb, sb2, teff, teff2 );
           REQUIRE( 3.0756984E-10 == Approx( sigVal ).epsilon(1e-6) );
           
+          e = 17;
+          ep = 22;
+          tev = 5.2;
+          sb = 13;
+          teff = 20.5;
+          alpha[0] = 1.9;
+          sigVal = sig( e, ep, u, tev, tevz, alpha, beta, sab, az, az2, lat, iinc, lasym, cliq, sb, sb2, teff, teff2 );
+          REQUIRE( 7.41596058E-2 == Approx( sigVal ).epsilon(1e-6) );
+
         } // THEN
       } // AND WHEN
     } // WHEN
+
+    // 150B 155 160
+    WHEN( "150B"){
+      e = 17;
+      ep = 22;
+      tev = 5.2;
+      sb = 13;
+      teff = 20.5;
+      lasym = 1;
+      cliq = 0.25;
+      alpha[0] = 1.9;
+      sigVal = sig( e, ep, u, tev, tevz, alpha, beta, sab, az, az2, lat, iinc, lasym, cliq, sb, sb2, teff, teff2 );
+      REQUIRE( 7.4159605806980167E-002 == Approx( sigVal ).epsilon(1e-6) );
+    } // WHEN
+
+    // 150C 155 160
+    WHEN( "150C"){
+      e = 17;
+      ep = 22;
+      tev = 4.2;
+      sb = 13;
+      teff = 20.5;
+      lasym = 0;
+      cliq = 0.25;
+      alpha[4] = 9.9;
+      sigVal = sig( e, ep, u, tev, tevz, alpha, beta, sab, az, az2, lat, iinc, lasym, cliq, sb, sb2, teff, teff2 );
+      REQUIRE( 4.159854584E-10 == Approx( sigVal ).epsilon(1e-6) );
+    } // WHEN
+
+    // 160
+    WHEN( "160"){
+      e = 17;
+      ep = 22;
+      tev = 28.2;
+      sb = 13;
+      teff = 20.5;
+      lasym = 0;
+      cliq = 0.25;
+      alpha[4] = 9.9;
+      sigVal = sig( e, ep, u, tev, tevz, alpha, beta, sab, az, az2, lat, iinc, lasym, cliq, sb, sb2, teff, teff2 );
+      REQUIRE( 0.79917055155412087 == Approx( sigVal ).epsilon(1e-6) );
+    } // WHEN
+
+
+
 
     // 170
     WHEN( "straight to 170" ){
@@ -68,9 +121,11 @@ TEST_CASE( "compute differential scattering ccross sections" ){
       REQUIRE( 3.370886372E-2 == Approx( sigVal ).epsilon(1e-6) );
     } // WHEN
 
-    */
 
+    // 200
     WHEN( "iinc != 2 " ){
+
+      // 200
       AND_WHEN( "iinc == 1" ){
         iinc = 1;
         e = 12; 
@@ -91,11 +146,11 @@ TEST_CASE( "compute differential scattering ccross sections" ){
 
 
       } // AND WHEN
+      // 200 300
       AND_WHEN( "iinc != 1" ){
         iinc = 3;
         sigVal = sig( e, ep, u, tev, tevz, alpha, beta, sab, az, az2, lat, iinc, lasym, cliq, sb, sb2, teff, teff2 );
         REQUIRE( 0.0 == Approx( sigVal ).epsilon(1e-6) );
-      //  std::cout << sigVal << std::endl;
 
       } // AND WHEN
 
@@ -127,7 +182,7 @@ TEST_CASE( "compute differential scattering ccross sections" ){
 
     } // WHEN
     // 170D
-    WHEN( "lasym = 0 and 170C" ){
+    WHEN( "lasym = 0 and 170D" ){
       e = 17;
       ep = 22;
       tev = 5.2;
@@ -141,4 +196,32 @@ TEST_CASE( "compute differential scattering ccross sections" ){
     } // WHEN
 
   } // GIVEN
+  GIVEN( "lat = 1" ){
+
+      lat = 1;
+    // 170
+    WHEN( "straight to 170" ){
+      e = 15;
+      ep = 10;
+      tev = 1.0;
+      sb = 15;
+      teff = 1.5;
+      sigVal = sig( e, ep, u, tev, tevz, alpha, beta, sab, az, az2, lat, iinc, lasym, cliq, sb, sb2, teff, teff2 );
+      REQUIRE( 0.6722681752 == Approx( sigVal ).epsilon(1e-6) );
+      //std::cout << sigVal << std::endl;
+      //
+      sb = 10;
+      teff = 80.5;
+      sigVal = sig( e, ep, u, tev, tevz, alpha, beta, sab, az, az2, lat, iinc, lasym, cliq, sb, sb2, teff, teff2 );
+      REQUIRE( 6.289982889E-2 == Approx( sigVal ).epsilon(1e-6) );
+
+      teff = 280.5;
+      sigVal = sig( e, ep, u, tev, tevz, alpha, beta, sab, az, az2, lat, iinc, lasym, cliq, sb, sb2, teff, teff2 );
+      REQUIRE( 3.370886372E-2 == Approx( sigVal ).epsilon(1e-6) );
+    } // WHEN
+
+
+
+
+  } //GIVEN
 } // TEST CASE
