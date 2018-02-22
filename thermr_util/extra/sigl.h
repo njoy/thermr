ae0e52dc6e7d63943a4ae5921c6c201712064fd7
Amelia Trainer <ameliajo@mit.edu>
2018-02-22 13:31:35 -0500

    small housekeeping for sigl

--- thermr_util/extra/sigl.h
+++ thermr_util/extra/sigl.h
@@ -19,19 +19,19 @@ auto do190( double& yl, std::vector<double>& y, std::vector<double>& x,
   if (nlin >= 0){
     legndr(xbar,p);
     for ( int il = 1; il < nl; ++il ){
-      s[il]=s[il]+p[il]/nbin;
+      s[il] = s[il] + p[il]/nbin;
     }
   } 
   // output equally probable angles
   else{
-     s[j+1-1]=xbar;
+     s[j] = xbar;
   }
 
   // continue bin loop and linearization loop
-  xl=xn;
-  yl=yn;
-  sum=0;
-  gral=0;
+  xl = xn;
+  yl = yn;
+  sum = 0;
+  gral = 0;
   if (j == nbin) {
     //   std::cout << "go to 260" << std::endl;
     return 260;
@@ -91,7 +91,7 @@ auto do170( double& xn, double& xl, int& j, double& yl, double& sigmin, double& 
    double yn=yl+(y[i-1]-yl)*(xn-xl)*xil;
    gral=gral+(xn-xl)*(yl*0.5*(xn+xl)
      +(y[i-1]-yl)*xil*(-xl*0.5*(xn+xl)
-     +(1/3)*(xn*xn+xn*xl+xl*xl)));
+     +(1.0/3)*(xn*xn+xn*xl+xl*xl)));
    double xbar=gral/ fract;
 
    // compute legendre components
@@ -157,12 +157,11 @@ auto do160( double& sum, double& xl, double& yl, std::vector<double>& x,
 
      }
      sum += add;
-     gral+=0.5*(yl*x[i-1]-y[i-1]*xl)*(x[i-1]+xl)
-       +(1.0/3)*(y[i-1]-yl)*(x[i-1]*x[i-1]+x[i-1]*xl+xl*xl);
+     gral+= 0.5*(yl*x[i-1]-y[i-1]*xl)*(x[i-1]+xl)
+       +(1.0/3.0)*(y[i-1]-yl)*(x[i-1]*x[i-1]+x[i-1]*xl+xl*xl);
    }
 
    // 250 continue
-   //std::cout << "250" << "     " << x[i-1] << "    " << i  << std::endl;
    xl = x[i-1];
    yl = y[i-1];
    i = i - 1;
@@ -193,72 +192,67 @@ auto sigl( double e, double ep, double tev, std::vector<double> alpha,
   *-------------------------------------------------------------------
   */
   
-  int nl,i,j,il,nbeta = beta.size(), nalpha = alpha.size();
-  double b,seep,sum,xl,yl,ymax,xm,ym,test,test2,fract,gral,add,xil,xn,f,rf,disc,yn,xbar,yt,tol,s1bb,xtol=.00001e0, ytol=.001e0, sigmin=1.e-32, eps=1.e-3, shade=.99999999e0;
-  int imax=20;
+  int nl,i,j,il,nbeta = beta.size(), nalpha = alpha.size(), imax = 20;
+  double b,sum,xl,yl,ymax,xm,ym,test,test2,fract,gral,add,xil,xn,f,rf,
+         disc,yn,xbar,yt,xtol=1e-5, ytol=1e-3, sigmin=1.e-32, 
+         eps=1.e-3, shade=.99999999e0;
   std::vector<double> x(imax), y(imax),p(nlin);
 
    // constant factors
-   b=(ep-e)/tev;
-   tol=0.5*tolin;
-   nl=nlin;
-   if (nl < 0) nl=-nl;
-   b=std::abs(b);
-   if (lat == 1 and iinc == 2) b=b*tev/tevz;
-   s1bb=sqrt(1+b*b);
-   if (ep != 0) seep=1/sqrt(e*ep);
+   nl = std::abs(nlin);
+   b = ( lat == 1 and iinc == 2 ) ? std::abs( (ep-e) / tevz ) :
+                                    std::abs( (ep-e) / tev  );
 
    // adaptive calculation of cross section
-   i=3;
-   sum=0;
-   x[2]=-1;
-   xl=x[2];
-   y[2]=sig(e,ep,x[2],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
-   yl=y[2];
-   if (ep == 0) x[1]=0;
-   if (ep != 0) x[1]=0.5*(e+ep-(s1bb-1)*az*tev)*seep;
-   if (std::abs(x[1]) > 1-eps) x[1]=0.99e0;
-   y[1]=sig(e,ep,x[1],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
-   x[0]=1;
-   y[0]=sig(e,ep,x[0],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
-   ymax=y[1];
-   if (y[0] > ymax) ymax=y[0];
-   if (y[2] > ymax) ymax=y[2];
-   if (ymax < eps) { ymax=eps; }
+   i = 3;
+   sum = 0;
+
+   x[0] = 1;
+   x[1] = (ep == 0) ? 0 : ( e + ep - (sqrt(1+b*b)-1) * az * tev ) / 
+                          ( 2 * sqrt(e*ep) );
+   if (std::abs(x[1]) > 1-eps) x[1] = 0.99;
+   x[2] = -1;
+
+   xl = x[2];
+   yl = y[2];
+
+   y[0] = sig(e,ep,x[0],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
+   y[1] = sig(e,ep,x[1],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
+   y[2] = sig(e,ep,x[2],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
+
+   ymax =  std::max(y[0],std::max(y[1],std::max(y[2],eps))); 
 
   //110 continue
   // if (i == imax) go to 120
    while (true){ 
      while ( true ){
-     std::cout << "110" << std::endl;
-     xm=0.5*(x[i-1]+x[i-2]);
-     ym=0.5*(y[i-1]+y[i-2]);
-     yt=sig(e,ep,xm,tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
-     test=tol*std::abs(yt)+tol*ymax/50;
-     test2=ym+ymax/100;
-     if (std::abs(yt-ym) <= test and std::abs(y[i-2]-y[i-1]) <= test2 and 
-       (x[i-2]-x[i-1]) < 0.5){
-       break;
+       std::cout << "110" << std::endl;
+       xm = 0.5*(x[i-1]+x[i-2]);
+       ym = 0.5*(y[i-1]+y[i-2]);
+       yt = sig(e,ep,xm,tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
+       test = 0.5 * tolin * ( std::abs(yt) + ymax / 50.0 );
+       test2 = ym + ymax / 100;
+       if ( ( std::abs(yt-ym)         <= test  and 
+              std::abs(y[i-2]-y[i-1]) <= test2 and 
+              (x[i-2]-x[i-1]) < 0.5 ) or 
+            ( x[i-2] - x[i-1] < xtol ) ){
+         break;
+       }
+       else {
+         i += 1;
+         x[i-1] = x[i-2]; x[i-2] = xm;
+         y[i-1] = y[i-2]; y[i-2] = yt;
+       }
      }
-     if (x[i-2]-x[i-1] < xtol) {
-       break;
-     }
-     i = i + 1;
-     x[i-1]=x[i-2];
-     y[i-1]=y[i-2];
-     x[i-2]=xm;
-     y[i-2]=yt;
-   }
-   while (true) {
-     std::cout << "120" << std::endl;
-     // 120 continue
-     sum = sum+0.5*(y[i-1]+yl)*(x[i-1]-xl);
-     xl=x[i-1];
-     yl=y[i-1];
-     i=i-1;
+     do {
+       std::cout << "120" << std::endl;
+       // 120 continue
+       sum += 0.5*(y[i-1]+yl)*(x[i-1]-xl);
+       xl = x[i-1];
+       yl = y[i-1];
+       i -= 1;
+     } while ( i == 1 );
 
-     if ( i != 1 ){ break; }
-   }
    if ( i < 1 ){ break; }
   }
    s[0]=sum;
@@ -278,55 +272,44 @@ auto sigl( double e, double ep, double tev, std::vector<double> alpha,
    for ( int il = 1; il < nl; ++il ){ 
      s[il] = 0; 
    }
-   j=0;
+   j = 0;
 
    // adaptive linearization
+   x[0] = 1;
+   x[1] = ep == 0 ? 0 : 0.5*(e+ep-(sqrt(1+b*b)-1)*az*tev)/sqrt(e*ep);
+   if (abs(x[1]) > 1-eps) { x[1] = 0.99; }
+   x[2] = -1;
+
    i = 3;
-   x[2]=-1;
-   xl=x[2];
-   y[2]=sig(e,ep,x[2],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
-   if (ep == 0 ) x[1]=0;
-   if (ep != 0 ) x[1]=0.5*(e+ep-(s1bb-1)*az*tev)*seep;
-   if (abs(x[1]) > 1-eps) x[1]=0.99e0;
-   y[1]=sig(e,ep,x[1],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
-   x[0]=+1;
-   y[0]=sig(e,ep,x[0],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
-   ymax=y[0];
-   if (y[1] > ymax) ymax=y[1];
-   if (y[2] > ymax) ymax=y[2];
-   if (ymax < eps) ymax=eps;
-   
-   int counter = 0;
+   xl = x[2];
+
+   y[0] = sig(e,ep,x[0],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
+   y[1] = sig(e,ep,x[1],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
+   y[2] = sig(e,ep,x[2],tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
+
+   ymax =  std::max(y[0],std::max(y[1],std::max(y[2],eps))); 
+
+
    while (true){
      while (true){
        //150 continue
        std::cout << "150" << std::endl;
        if (i == imax) { std::cout << "go to 160!!!" << std::endl; break; }
        else {
-         //if (i == imax) go to 160
-         xm=0.5*(x[i-1]+x[i-2]);
-         ym=0.5*(y[i-1]+y[i-2]);
-         //std::cout << "----------------- " << i << "   " << xm << "    " << ym  << std::endl;
+         xm = 0.5*(x[i-1]+x[i-2]);
+         ym = 0.5*(y[i-1]+y[i-2]);
          yt=sig(e,ep,xm,tev,tevz,alpha,beta,sab,az,az2,lat,iinc,lasym,cliq,sb,sb2,teff,teff2);
-     //std::cout << x[i-2]  << "   " << x[i-1] << std::endl;
-         test=tol*std::abs(yt)+tol*ymax/50;
-         test2=ym+ymax/100;
-         if (abs(yt-ym) <= test and 
-             std::abs(y[i-1]-y[i-2]) <= test2 and 
-            (x[i-2]-x[i-1]) < 0.5) {
+         test = 0.5 *tolin * ( std::abs(yt) + ymax/50.0 );
+         test2 = ym + ymax / 100.0;
+         if ( ( std::abs(yt-ym)         <= test  and 
+                std::abs(y[i-1]-y[i-2]) <= test2 and 
+                (x[i-2]-x[i-1]) < 0.5) or 
+              ( x[i-2]-x[i-1] < xtol ) ) {
             break; 
          }
-         if (x[i-2]-x[i-1] < xtol){
-            break; 
-         }
-         i=i+1;
-         x[i-1]=x[i-2];
-         y[i-1]=y[i-2];
-         x[i-2]=xm;
-         y[i-2]=yt;
-      //std::cout << "    " << x[0] << "     " << x[1] << "      " << x[2] << "    " << i << "   " << j << std::endl;
-      counter +=1 ;
-      if (counter > 3 ){return;}
+         i += 1;
+         x[i-1] = x[i-2]; x[i-2] = xm;
+         y[i-1] = y[i-2]; y[i-2] = yt;
          //go to 150
        }
      }
