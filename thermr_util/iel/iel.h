6f1838537efe62ff6065a7af192bd010463b8c7b
Amelia Trainer <ameliajo@mit.edu>
2018-03-13 14:51:36 -0400

    commented and cleaned up iel, needs some endf functions

--- thermr_util/iel/iel.h
+++ thermr_util/iel/iel.h
@@ -1,5 +1,5 @@
-
 #include "terp.h"
+#include "terpa.h"
 
 auto iel( int mat, int itemp, int iold, int inew, int ne, int nex, 
     std::vector<double>& tempr, std::vector<double>& fl, int za, 
@@ -7,6 +7,8 @@ auto iel( int mat, int itemp, int iold, int inew, int ne, int nex,
     std::vector<double>& esi 
     ){
   /*-------------------------------------------------------------------
+   * INCOHERENT ELASTIC SCATTERING
+   *-------------------------------------------------------------------
    * Compute the elastic scattering from polyethylene or hydrogen
    * in zirconium hydride using the incoherent approximation.
    * The calcem energy grid is used.  If mat == 11 or 12, built-in
@@ -14,12 +16,11 @@ auto iel( int mat, int itemp, int iold, int inew, int ne, int nex,
    * If mat == 20, material parameters have been read from ENDF6.
    *-------------------------------------------------------------------
    */
-   int idis,iex,iet,iu,ix,nj,nr,np,ip,ir,ltt,nb,nw,matdp;
+   int idis,iex,iet,ix,nj,nr,np,ip,ir,ltt,nb,nw,matdp;
    int n,nup,nup1,isave,nne;
-   double dwa,c1,e,c2,u,rc2,temp,tt1,ttn,tnxt,math,mth,mfh,mtref,sb;
-   double x1,r1x1,xsec,x2,unow;
+   double dwa,c1,e,c2,uLeft,rc2,temp,tt1,ttn,tnxt,math,mth,mfh,mtref,sb;
+   double x1,r1x1,xsec,x2,uRight;
    std::vector<double> ex(20), ej(20);
-   std::vector<double> xie;
    int nupmax = 10;
    std::vector<double> tmp { 296,400,500,600,700,800,1000,1200 },
      dwh { 8.4795, 9.0854, 9.8196, 10.676, 11.625, 12.643, 14.822, 17.125 },
@@ -32,28 +33,28 @@ auto iel( int mat, int itemp, int iold, int inew, int ne, int nex,
    nj=nex+1;
 
    // select material parameters
-   if (mat == 11) {
-      sb=c11a;
-      dwa=c11c+(temp-c11b)*(c11e-c11c)/(c11d-c11b);
+   if (mat == 11) {          // D in D2O
+      sb=c11a;  // Characteristic bound cross section
+      dwa=c11c+(temp-c11b)*(c11e-c11c)/(c11d-c11b); // Debye-Waller Coefficient
    }
-   else if (mat == 12) {
-      dwa=terp(tmp,dwh,8,temp,3);
-      sb=c12a;
+   else if (mat == 12) {     // D in para D2O
+      dwa=terp(tmp,dwh,8,temp,3);                   // Deybe-Waller Coefficient
+      sb=c12a;  // Characteristic bound cross section
    }
-   else if (mat == 13) {
-      dwa=terp(tmp,dwz,8,temp,3);
-      sb=c13a;
+   else if (mat == 13) {     // D in ortho D2O
+      dwa=terp(tmp,dwz,8,temp,3);                   // Debye-Waller Coefficient
+      sb=c13a;  // Characteristic bound cross section
    }
    else if (mat == 20) {
-      sb=fl[1];
-      nr=round(fl[5-1]);
-      np=round(fl[6-1]);
+      sb=fl[0]; // Characteristic bound cross section
+      nr=round(fl[4]);
+      np=round(fl[5]);
       if (np == 1) {
          tt1=fl[7+2*nr-1];
          if (abs(temp-tt1) > temp/10){
            std::cout << "call error('iel',&'bad temperature for debye-waller factor',' ') " << std::endl;
          }
-         dwa=fl[8+2*nr];
+         dwa=fl[8+2*nr];                            // Debye-Waller Coefficient
       }
       else {
          tt1=fl[7+2*nr-1];
@@ -65,7 +66,9 @@ auto iel( int mat, int itemp, int iold, int inew, int ne, int nex,
          if (ttn < temp) fl[5+2*nr+2*np]=temp;
          ip=2;
          ir=1;
-         //call terpa(dwa,temp,tnxt,idis,fl,ip,ir)
+         auto out = terpa(dwa,temp,fl,ip,ir);
+         tnxt = std::get<0>( out ); 
+         idis = std::get<1>( out );
       }
    } 
    else {
@@ -80,6 +83,7 @@ auto iel( int mat, int itemp, int iold, int inew, int ne, int nex,
      nne=nne+1;
    }
    //allocate(xie(nne))
+   std::vector<double> xie( nne );
 
    // write head and tab2 records for mf6
    // in lanl format
@@ -124,59 +128,57 @@ auto iel( int mat, int itemp, int iold, int inew, int ne, int nex,
    nup1=nup+1;
    nw=n+8;
 
-   /*
    // compute cross sections on calcem energy grid
    // and equi-probable angles.
-   do iex=1,nne
-      e=esi(iex)
-      c2=2*e*dwa
-      u=-1
-      rc2=1/c2
-      x1=exp(-2*c2)
-      r1x1=1/(1-x1)
-      xsec=c1*rc2*(1-x1)
-      scr[1)=0
-      scr[2)=e
-      scr[3)=0
-      scr[4)=0
-      scr[5)=n+2
-      scr[6)=n+2
-      scr[7)=e
-      scr[8)=1
-      do iu=1,n
-         x2=exp(-c2*(1-u))
-         unow=1+rc2*log((1-x1)/n+x2)
-         scr[8+iu)=n*rc2*(exp(-c2*(1-unow))*(c2*unow-1)-&
-           x2*(c2*u-1))*r1x1
-         u=unow
-      enddo
-      xie(iex)=xsec
-      if (iprint == 2) write(nsyso,'(4x,1p,2e12.4,0p,10f9.4)')&
-        e,xsec,(scr[8+iu),iu=1,nup)
-      if (iprint == 2 and n > nup)&
-        write(nsyso,'(28x,10f9.4)') (scr[8+iu),iu=nup1,n)
-      call listio(0,0,nscr,scr,nb,nw)
-   enddo
-   do iex=1,ne
-      call finda(iex,ex,nex,iold,bufo,nbuf)
-      do ix=1,nex
-         ej(ix)=ex(ix)
-      enddo
-      ej(nj)=terp(esi,xie,nne,ex(1),3)
-      if (iex == ne) ej(nj)=0
-      iet=iex
-      if (iex == ne) iet=-iet
-      call loada(iet,ej,nj,inew,bufn,nbuf)
-   enddo
-   isave=iold
-   iold=inew
-   inew=isave
-   nex=nj
-   call asend(0,nscr)
-   ncdse=5+ne*((n+11)/6)
-   return
-   end subroutine iel
+   for ( int iex = 0; iex < nne; ++iex ){
+      e = esi[iex];
+      c2 = 2*e*dwa;
+      uLeft=-1;
+      rc2=1/c2;
+      x1=exp(-4*e*dwa);
 
-   */
+      // This is the incoh. elastic xs sigma from Eq. 232. This is assuming
+      // equiprobable angles.
+      xsec = (sb*0.5) * ( 1.0 - exp(-4.0*e*dwa) ) / ( 2.0*e*dwa );
+
+      scr[1] = 0;     scr[2] = e; scr[3] = 0; scr[4] = 0; scr[5] = n + 2;
+      scr[6] = n + 2; scr[7] = e; scr[8] = 1;
+
+      for ( int iu = 0; iu < n; ++iu ){
+
+         x2 = exp(-(2*e*dwa)*(1-uLeft));
+         // This is mu_i of Eq. 234
+         uRight = 1 + 1/(2*e*dwa) * log( ( 1 - exp(-4*e*dwa) ) / n + 
+                                         exp( -2*e*dwa*(1-uLeft) ) );
+
+         // This is Eq. 233, and represents the selected discrete cosine of the
+         // ith bin
+         scr[8+iu] = n / ( 2*e*dwa * ( 1 - exp(-4*e*dwa) ) ) *
+                     ( exp( -2*e*dwa*(1-uRight) ) * ( 2*e*dwa*uRight-1 )
+                     - exp( -2*e*dwa*(1-uLeft ) ) * ( 2*e*dwa*uLeft -1 ) );
+ 
+         uLeft = uRight;
+      }
+
+      xie[iex] = xsec;
+      //call listio(0,0,nscr,scr,nb,nw)
+   }
+   for ( int iex = 0; iex < ne; ++iex ){
+      // call finda(iex,ex,nex,iold,bufo,nbuf)
+      for ( int ix = 0; ix < nex; ++ix ){
+         ej[ix]=ex[ix];
+      }
+      ej[nj] = (iex == ne) ? 0 : terp(esi,xie,nne,ex[0],3);
+      iet    = (iex == ne) ? -iex : iex;;
+      //call loada(iet,ej,nj,inew,bufn,nbuf)
+   }
+   isave=iold;
+   iold=inew;
+   inew=isave;
+   nex=nj;
+   //call asend(0,nscr)
+   auto ncdse=5+ne*((n+11)/6);
+   return;
+
 }
 
