a5f334a595dae284687fcad50acececc86ff7242
Amelia Trainer <ameliajo@mit.edu>
2018-03-13 11:22:52 -0400

    added tests and cleaned up terpa

--- thermr_util/iel/terpa.h
+++ thermr_util/iel/terpa.h
@@ -1,115 +1,95 @@
 
-auto terpa(double y, double x, double xnext, int idis,
+auto terpa(double y, double x, int idis,
     std::vector<double> a, int ip, int ir){
-  /*--------------------------------------------------------------------
-   * Interpolate for y(x) in the TAB1 record packed in a.  Return  0 
-   * if x is outside the range of the table.   Here xnext is the next
-   * data grid point greater than x.  On entry, ip and ir are starting
-   * estimates for the first data point greater than x and the
-   * corresponding interpolation range.  Initialize them to 2 and 1
-   * before first call to routine.
-   *--------------------------------------------------------------------
-   */
-   int nr,np,jr,jp,intVar,it;
-   double shade=1.00001, xbig = 1e12;
+ /*--------------------------------------------------------------------
+  * Interpolate for y(x) in the TAB1 record packed in a.  Return  0 
+  * if x is outside the range of the table.   Here xnext is the next
+  * data grid point greater than x.  On entry, ip and ir are starting
+  * estimates for the first data point greater than x and the
+  * corresponding interpolation range.  Initialize them to 2 and 1
+  * before first call to routine.
+  *--------------------------------------------------------------------
+  */
+  int nr,np,jr,jp,intVar,it;
+  double shade=1.00001, xbig = 1.0e12, xnext;
 
-   // set up limits and pointers
-   nr=round(a[5-1]);
-   np=round(a[6-1]);
-   if (ir > nr) ir=nr;
-   if (ip > np) ip=np;
-   jr=5+2*ir;
-   jp=5+2*nr+2*ip;
-   idis=0;
+  // set up limits and pointers
+  nr=round(a[4]);
+  np=round(a[5]);
+  if (ir > nr) ir=nr;
+  if (ip > np) ip=np;
+  jr=5+2*ir;
+  jp=5+2*nr+2*ip;
+  idis=0;
 
-   int counter = 0;
-   while (true) {
-     ++counter;
-     // locate interpolation interval and law for x
-     // 110 continue
-     std::cout << "110  " << ip << "    " << np << std::endl;
-     if (x < a[jp-1]){
-       std::cout << "120" << std::endl;
-       //go to 120
-       //120 continue
-       if (x > a[jp-2-1]){
-         std::cout << "130" << std::endl;
-         // go to 130
-         // interpolate for y in this interval
-         // 130 continue
-         intVar=round(a[jr+1-1]);
-         // call terp1(a(jp-2),a(jp-1),a(jp),a(jp+1),x,y,intVar);
-         xnext=a[jp-1];
-         if (intVar == 1) idis=1;
-         if (ip == np) return;
-         if (a[jp+2-1] == xnext) idis=1;
-         return;
-       }
-       // go to 140
-       std::cout << "140" << std::endl;
-       if (x == a[jp-2-1]) {
-         // 140 continue
-         y=a[jp-1-1];
-         intVar=round(a[jr+1-1]);
-         xnext=a[jp-1];
-         if (intVar == 1) idis=1;
-         if (ip == np) return;
-         if (a[jp+2-1] == xnext) idis=1;
-         return;
-       }
-       // go to 170
-       if (ip == 2) {
-         // special branch for x below first point
-         // 170 continue
-         std::cout << "170" << std::endl;
-         y=0;
-         xnext=a[jp-2-1];
-         idis=1;
-         return;
-       }
-       // move down
-       jp=jp-2;
-       ip=ip-1;
-       if ( ir != 1 ){
-         it=round(a[jr-2-1]);
-         if ( ip <= it ){
-           jr=jr-2;
-           ir=ir-1;
-         }
-       }
-       //go to 110
-     }
-     if (counter > 15 ){ return; }
+  while (true) {
+    // locate interpolation interval and law for x
+    std::cout << "110  " << ip << "    " << jp << "    " << a[jp-1] << std::endl;
+    if (x < a[jp-1]){
+      std::cout << "120" << std::endl;
+      if (x > a[jp-3]){
+        std::cout << "130" << std::endl;
+        // interpolate for y in this interval
+        intVar=round(a[jr+1-1]);
+        // call terp1(a(jp-2),a(jp-1),a(jp),a(jp+1),x,y,intVar);
+        xnext=a[jp-1];
+        if (intVar == 1) idis=1;
+        if (ip == np) return xnext;
+        if (a[jp+1] == xnext) idis=1;
+        return xnext;
+      }
+      if (x == a[jp-3]) {
+        std::cout << "140" << std::endl;
+        y=a[jp-1-1];
+        intVar=round(a[jr+1-1]);
+        xnext=a[jp-1];
+        if (intVar == 1) idis=1;
+        if (ip == np) return xnext;
+        if (a[jp+1] == xnext) idis=1;
+        return xnext;
+      }
+      if (ip == 2) {
+        // special branch for x below first point
+        std::cout << "170" << std::endl;
+        y = 0;
+        idis = 1;
+        return a[jp-3];
+      }
+      // move down
+      jp=jp-2;
+      ip=ip-1;
+      if ( ir != 1 ){
+        it=round(a[jr-3]);
+        if ( ip <= it ){
+          jr = jr-2;
+          ir = ir-1;
+        }
+      }
+      //go to 110
+    }
 
-   if (ip == np) {
-    std::cout << "150" << std::endl;
-    // go to 150
-    // special branch for last point and above
-    // 150 continue
-    if (x < shade*a[jp-1]) {
-       std::cout << "160" << std::endl;
-       // go to 160
-       // 160 continue
-       y=a[jp+1-1];
-       xnext=xbig;
-       if (y >  0 ) xnext=shade*shade*a[jp-1];
-       return;
-     }
-     y=0;
-     xnext=xbig;
-     return;
-   }
+    if (ip == np) {
+      std::cout << "150" << std::endl;
+      // special branch for last point and above
+      if (x < shade*a[jp-1]) {
+         std::cout << "160" << std::endl;
+         y = a[jp];
+         return ( y > 0 ) ? shade * shade * a[jp-1] : xbig;
+      }
+      y=0;
+      return xbig;
+    }
 
-   // move up
-   jp=jp+2;
-   ip=ip+1;
-   it=round(a[jr-1]);
-   if ( ip < it ){
-     jr=jr+2;
-     ir=ir+1;
-   }
-   //go to 110
- }
+    // move up
+    jp=jp+2;
+    ip=ip+1;
+    it=round(a[jr-1]);
+    if ( ip < it ){
+      jr=jr+2;
+      ir=ir+1;
+    }
+    //go to 110
+  }
 }
 
 
--- thermr_util/iel/terpa.test.cpp
+++ thermr_util/iel/terpa.test.cpp
@@ -6,15 +6,59 @@
 TEST_CASE( "terpa" ){
   double x, y, xnext;
   int idis, ip, ir;
-  std::vector<double> a { 1.1, 2.2, 3.3, 5.5, 8.8, 13.13, 21.21, 34.34 };
 
-  x = 1; y = 2; idis = 3; ip = 2; ir = 1;
-  terpa( y, x, xnext, idis, a, ip, ir );
-  std::cout << "---------   " <<  xnext << std::endl;
-  
-
-  GIVEN( ""  ){
+  GIVEN( "small values in input vector" ){
+    THEN( "110 multiple times then 150, defaulting to xbig" ){
+      std::vector<double> a { 1.1, 2.2, 3.3, 5.5, 8.8, 13.13, 21.21, 34.34 };
+      x = 1; y = 2; idis = 3; ip = 2; ir = 1;
+      xnext = terpa( y, x, idis, a, ip, ir );
+      REQUIRE( 1.0e12 == Approx( xnext ).epsilon(1e-6) );
+    } // THEN
 
   } // GIVEN
+
+  GIVEN( "x value is not equal to a value in the a vector" ){
+    THEN( "110 then 120 and 130" ){
+      std::vector<double> a (100);
+      for ( int i = 0; i < 100; ++i ){ a[i] = 20 + i * 0.1; }
+      x = 2; y = 1; idis = 3; ip = 2; ir = 1;
+      xnext = terpa( y, x, idis, a, ip, ir );
+      REQUIRE( 24.6 == Approx( xnext ).epsilon(1e-6) );
+
+      idis = 300; 
+      xnext = terpa( y, x, idis, a, ip, ir );
+      REQUIRE( 24.6 == Approx( xnext ).epsilon(1e-6) );
+
+    } // THEN
+
+  } // GIVEN
+  GIVEN( "x is equal to a value in the a vector " ){
+    THEN( "110 then 120 and 140" ){
+      std::vector<double> a (100);
+      for ( int i = 0; i < 100; ++i ){ a[i] = 20 + i * 0.1; }
+      x = 25; y = 1; idis = 3; ip = 2; ir = 1;
+      xnext = terpa( y, x, idis, a, ip, ir );
+      REQUIRE( 25.2 == Approx( xnext ).epsilon(1e-6) );
+
+    } // THEN
+
+  } // GIVEN
+  GIVEN( " " ){
+    THEN( "110 immediately to 150" ){
+      std::vector<double> a { 2.01, 2.02, 2.03, 2.05, 2.08, 2.13, 2.21, 2.34, 
+        0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 };
+      x = 12.5; y = 1; idis = 3; ip = 2; ir = 1;
+      xnext = terpa( y, x, idis, a, ip, ir );
+      REQUIRE( 1.0e12 == Approx( xnext ).epsilon(1e-6) );
+      std::cout << xnext << std::endl;
+
+    } // THEN
+
+  } // GIVEN
+ 
+ 
+ 
+  
+
 
 } // TEST CASE
