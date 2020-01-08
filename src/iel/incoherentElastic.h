3c2c1ec34a6764d023490517efd30fd7a8113156
Amelia Trainer <ameliajo@mit.edu>
2020-01-08 14:52:42 -0500

    removed xs from the cosine calc in incoh elastic. moved to own function because they won't be on the same grid

--- src/iel/incoherentElastic.h
+++ src/iel/incoherentElastic.h
@@ -1,27 +1,49 @@
 #include <iostream>
 
 template <typename Float, typename Range>
-auto getIncohElasticEquiprobableAngles( const Float& E, const Float& dwf,
-  const Float& sigma_b, int nAtoms, Range& equiprobCosines){
+auto getIncohElasticDataSingleEnergy( const Float& E, const Float& dwf,
+  Range& equiprobCosines){
   
   int nAngles = equiprobCosines.size();
-  Float sigma = sigma_b/(nAtoms*4.0*dwf*E)*(1.0-exp(-4.0*dwf*E)),
-        lastCosine = -1.0, thisCosine, x2, mu_i, c2;
-
-  c2 = 2.0*E*dwf;
-  Float exp_4EW = exp(-4.0*E*dwf);
+  Float lastCosine = -1.0, thisCosine, exp_2EW_cos, mu_i,
+        exp_4EW = exp(-4.0*E*dwf),
+        inv_2EW = 1.0/(2.0*E*dwf);
 
   for ( int i = 0; i < nAngles; ++i ){
-    x2 = exp(-c2*(1.0-lastCosine));
-    thisCosine = 1.0+(1.0/c2)*log((1-exp_4EW)/nAngles + x2);
-    mu_i = nAngles/c2*(exp(-c2*(1.0-thisCosine))*(c2*thisCosine-1.0)-x2*(c2*lastCosine-1.0))/(1.0-exp_4EW);
+    exp_2EW_cos = exp( -2.0*E*dwf*(1.0-lastCosine) );
+    thisCosine  = 1.0+inv_2EW*log((1-exp_4EW)/nAngles + exp_2EW_cos );
+
+    mu_i = nAngles * inv_2EW * 
+           ( exp(-2.0*E*dwf*(1.0-thisCosine))*(2.0*E*dwf*thisCosine-1.0) - 
+             exp_2EW_cos*(2.0*E*dwf*lastCosine-1.0) ) / (1.0-exp_4EW);
     equiprobCosines[i] = mu_i;
     lastCosine = thisCosine;
   }
   // Return the scattering cross section sigma(E)
-  return sigma_b/(2.0*nAtoms) * (1.0-exp_4EW)/c2;
+}
+
+
+template <typename Float>
+Float getIncohElasticXS( const Float& sigma_b, const Float& dwf, 
+  const Float& E, int nAtoms ){
+  return sigma_b/(2.0*nAtoms) * (1.0-exp(-4.0*E*dwf)) / ( 2.0*E*dwf ); 
+}
+
+
+
+/*
+
+template <typename Float, typename Range>
+auto getIncohElasticDataAllEnergies( const Range& Egrid, const Float& Emax, 
+  const Float& sigma_b, int nAtoms, int nBins ){
+  
+  for 
 
 }
+
+*/
+
+
 
 
 
