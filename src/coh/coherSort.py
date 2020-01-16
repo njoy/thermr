89f510c7854aa088cb7cb7d59584ff173891e738
Amelia Trainer <ameliajo@mit.edu>
2020-01-16 14:43:04 -0500

    made the quick python script prettier / functional

--- src/coh/coherSort.py
+++ src/coh/coherSort.py
@@ -1,42 +1,51 @@
 import numpy as np
 from random import random
 
-Egrid = [0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
-Egrid = [0.0, 1.0, 2.0, 3.0]
+Egrid = list(np.linspace(0,10,100))
 
 def f(x):
     return -x**2+5.0*x+2.0
 
 
-def sufficientlyClose(x1,x2,tol=1e-3):
+def sufficientlyClose(x1,x2,tol=1e-1):
     return ( abs(f(x1)-f(x2)) <= tol )
 
 
-
 braggs = sorted([random()*Egrid[-1] for i in range(1)])
-braggs = [2.3,2.9,3.1,3.5,6.1]
-braggs = [2.3]
 
 def findLocation(Egrid, bragg, j=0):
     for i in range(j,len(Egrid)-1):
         if Egrid[i+1] > bragg:
             return i
+    return len(Egrid)
+
+
+def addPoint(x1,x2,vec):
+    if (sufficientlyClose(x1,x2)):
+        #print(x1,'and',x2,'are sufficiently close!')
+        finalE.append(x2)
+    else:
+        #print("need to add a midpoint!")
+        xm = (x1+x2)*0.5
+        #print("midpoint = ",xm)
+        addPoint(x1,xm,vec)
+        #vec.append(xm)
+        addPoint(xm,x2,vec)
+
 
 
 finalE = []
-i_old = -1
-
+i_old = 0
+finalE = [Egrid[0]]
 for bragg in braggs:
     i = findLocation(Egrid,bragg)
     if ( i > i_old ):
-        finalE += Egrid[i_old+1:i+1]
-    finalE.append(bragg)
-
+        for k in range(i_old+1,i+1):
+            addPoint(finalE[-1],Egrid[k],Egrid)
+    addPoint(finalE[-1],bragg,finalE)
     i_old = i
 
-
 finalE += Egrid[i+1:]
-print(finalE)
 print( (([x in finalE for x in Egrid]==[True]*len(Egrid)) and \
         ([x in finalE for x in braggs]==[True]*len(braggs)) and \
         finalE == sorted(finalE)))
