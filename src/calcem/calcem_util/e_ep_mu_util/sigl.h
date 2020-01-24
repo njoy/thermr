4b647650321e827fae927b915906bd2296226f00
Amelia Trainer <ameliajo@mit.edu>
2020-01-24 12:43:22 -0500

    cleaner tests for sigl

--- src/calcem/calcem_util/e_ep_mu_util/sigl.h
+++ src/calcem/calcem_util/e_ep_mu_util/sigl.h
@@ -36,58 +36,52 @@ inline Float maxOf4Vals( const Float a, const Float b, const Float c, const Floa
 }
 
 template <typename Range, typename Float>
-auto populateXSvector(Range& xsVec, Float& xn, Float& invDeltaMu, Float& muLeft, 
-  Float& xsLeft, Float& fract, int& i, Float& gral, const int& nbin, Range& s, 
-  const int& j, bool equiprobableBins){
-  //std::cout << " --- 190 --- " << std::endl;
-  gral += (xn-muLeft) * 
-          ( xsLeft*0.5*(xn+muLeft) + 
-            (xsVec[i-1]-xsLeft)*invDeltaMu*(-muLeft*0.5*(xn+muLeft) + 
-            0.333333333*(xn*xn+xn*muLeft+muLeft*muLeft))
+auto populateXSvector(Range& xsVec, Float& mu, Float& invDeltaMu, Float& muLeft, 
+  Float& xsLeft, Float& xs_per_bin, int& i, Float& gral, const int& nbin, Range& s, 
+  const int& j, bool equiprobableBins){ //std::cout << " --- 190 --- " << std::endl;
+  gral += (mu-muLeft) * 
+          ( xsLeft*0.5*(mu+muLeft) + 
+            (xsVec[i-1]-xsLeft)*invDeltaMu*(-muLeft*0.5*(mu+muLeft) + 
+            0.333333333*(mu*mu+mu*muLeft+muLeft*muLeft))
           );
 
   if (equiprobableBins){
-    s[j-1] = gral / fract;  // xbar
+    s[j-1] = gral / xs_per_bin;  // xbar
   }
   else {        // Here we fill in the legendre components
     Range p (nbin+1,0.0);
-    legndr(gral/fract,p,nbin+1);
+    legndr(gral/xs_per_bin,p,nbin+1);
     for (int k = 1; k < nbin+1; ++k){ s[k-1] += p[k]/nbin; }
   }
 
-  xsLeft = xsLeft + (xsVec[i-1]-xsLeft)*(xn-muLeft)*invDeltaMu;
-  muLeft = xn;
+  xsLeft = xsLeft + (xsVec[i-1]-xsLeft)*(mu-muLeft)*invDeltaMu;
+  muLeft = mu;
   gral = 0.0;
 }
 
 
 template <typename Range, typename Float>
-auto getNextMuValue(Float& fract, const Float& sum, Range& xsVec, Range& muVec, 
-  Float& xsLeft, Float& muLeft, int& i, Float& invDeltaMu ){
-  //std::cout << " --- 170 --- " << std::endl;
-  Float xn;
-  Float test = (fract-sum)*(xsVec[i-1]-xsLeft)/((muVec[i-1]-muLeft)*xsLeft*xsLeft);
+auto getNextMuValue(Float& xs_per_bin, const Float& sum, Range& xsVec, 
+  Float& xsLeft, Float& muLeft, Float& muRight, int& i, Float& invDeltaMu ){
+  Float mu, //std::cout << " --- 170 --- " << std::endl;
+    test = (xs_per_bin-sum)*(xsVec[i-1]-xsLeft)/((muRight-muLeft)*xsLeft*xsLeft);
 
   if ( xsLeft >= 1e-32 and abs(test) <= 1e-3 ){ // Could potentially do 180, 190
-      xn = muLeft + (fract-sum)/xsLeft;
-      if ( muVec[i-1] < xn ){ //std::cout << " --- 180 --- " << std::endl;
-        return muVec[i-1];
-      } 
-      else if ( muLeft <= xn ){
-        return xn;
-      }
+      mu = muLeft + (xs_per_bin-sum)/xsLeft; //std::cout << " --- 180 --- " << std::endl;
+      if      ( muRight <  mu ){ return muRight; } 
+      else if ( muLeft  <= mu ){ return mu;      }
   }
   //std::cout << " --- 175 --- " << std::endl;
   Float f = (xsVec[i-1]-xsLeft)*invDeltaMu;
-  Float sqrt_disc = pow(pow((xsLeft/f),2)+2.0*(fract-sum)/f,0.5);
+  Float sqrt_disc = pow(pow((xsLeft/f),2)+2.0*(xs_per_bin-sum)/f,0.5);
 
-  xn = ( f > 0 ) ? muLeft - (xsLeft/f) + sqrt_disc 
+  mu = ( f > 0 ) ? muLeft - (xsLeft/f) + sqrt_disc 
                  : muLeft - (xsLeft/f) - sqrt_disc;
 
-  if ( xn <= muLeft ){ throw std::exception(); }
+  if ( mu <= muLeft ){ throw std::exception(); }
 
-  if ( xn <= muVec[i-1] ){ return xn; }
-  if ( xn < (muVec[i-1]+1e-3*(muVec[i-1]-muLeft))){ return muVec[i-1]; }
+  if ( mu <= muRight ){ return mu; }
+  if ( mu < (muRight+1e-3*(muRight-muLeft))){ return muRight; }
   else { throw std::exception(); }
 
 }
@@ -147,14 +141,16 @@ inline auto getPDF(Float ep, Float e, Float tev, Float tol, int lat, int iinc,
   // midpoint and i-1. If we don't need a midpoint, we go to the inner loop
   // and move to the left and keep checking. 
   int i = 3;
+  Float muRight = muVec[i-1];
   do {
     addMidpointsRight(i,muVec,xsVec,e,ep,tev,alphas,betas,sab,az,lasym,lat,
                       sigma_b,sigma_b2,teff,iinc,tol,xsMax);
     do { // If i = 2, then do this action twice. Else do it once
-      pdf += 0.5*( xsVec[i-1] + xsLeft )*( muVec[i-1] - muLeft );
+      pdf += 0.5*( xsVec[i-1] + xsLeft )*( muRight - muLeft );
       muLeft = muVec[i-1];
       xsLeft = xsVec[i-1];
       --i;
+      muRight = muVec[i-1];
     } while ( i == 1 );
   } while ( i > 1 );
   return pdf;
@@ -177,24 +173,23 @@ inline auto sigl(Float ep, Float e, Float tev, Float tol, int lat, int iinc,
   // Integrate the incoherent inelastic scattering xs over all mu values, so 
   // that we can later divide by the requested # of angle bins so that we know
   // how much to fill up each bin
-  Range muVec(20), xsVec(20); 
   Float pdf = getPDF(ep, e, tev, tol, lat, iinc, alphas, betas, sab, az, lasym, 
                      sigma_b, sigma_b2, teff);
-
 
   Range s(nbin,0.0);
   if ( pdf <= 1e-32 ){ return s; }
 
-
+  Range muVec(20), xsVec(20); 
   initialize_XS_MU_vecs(muVec,xsVec,e,ep,az,tev,alphas,betas,sab,lasym,lat,
                         sigma_b,sigma_b2,teff,iinc); // 130
 
   Float xsMax  = maxOf4Vals( xsVec[0], xsVec[1], xsVec[2], 0.001),
         muLeft = muVec[2],
+        muRight,
         xsLeft = xsVec[2],
         gral   = 0.0,
         sum    = 0.0,
-        fract  = pdf/(1.0*nbin); // about how much integrated xs we want per bin 
+        xs_per_bin  = pdf/(1.0*nbin); // about how much integrated xs we want per bin 
 
   int i = 3, j = 0;
 
@@ -202,58 +197,64 @@ inline auto sigl(Float ep, Float e, Float tev, Float tol, int lat, int iinc,
     addMidpointsRight(i,muVec,xsVec,e,ep,tev,alphas,betas,sab,az,lasym,lat,
                       sigma_b,sigma_b2,teff,iinc,tol,xsMax);
     do { //std::cout << " --- 160 ---  "<< std::endl;
-      if (muVec[i-1] == muLeft) {  
+      muRight = muVec[i-1];
+      if (muRight == muLeft) {  
         // Is this our first time in the outer do loop? i.e. are we trying to
         // compare -1 with -1? If so, let's just move i down one and try again.
         --i; //std::cout << " --- 250 ---  "<< std::endl;
+        muRight = muVec[i-1];
         if ( i < 1 ){ return s; }
         if ( i > 1 ){ break; }
       }
 
-      Float invDeltaMu = 1.0/(muVec[i-1]-muLeft);
+      Float invDeltaMu = 1.0/(muRight-muLeft);
 
       // This is the integrated little block of xs (from muLeft -> muVec[i-1]).
       // We're going to want to see if adding this to the sum will make us reach
-      // our desired xs from fract (recall that fract is the xs we will have 
+      // our desired xs from xs_per_bin (recall that xs_per_bin is the xs we will have 
       // in each bin if they're perfectly equiprobable).
-      Float add = 0.5*(xsVec[i-1]+xsLeft)*(muVec[i-1]-muLeft);
+      Float add = 0.5*(xsVec[i-1]+xsLeft)*(muRight-muLeft);
 
       if ( i == 1 and j == nbin-1 ){ //std::cout << " --- 165 ---  "<< std::endl;
         j++;
-        Float xn = muVec[i-1];
-        populateXSvector(xsVec, xn, invDeltaMu, muLeft, xsLeft, fract, i, gral, 
-                         nbin, s, j, equiprobableBins);
+        Float mu = muRight;
+        populateXSvector(xsVec, mu, invDeltaMu, muLeft, xsLeft, xs_per_bin, i, 
+                         gral, nbin, s, j, equiprobableBins);
         return s;
       }
-      else if ( sum + add >= fract * 0.99999999 and j < nbin-1 ){
-        // Here, we're trying to get sum+add as close to possible as fract. 
+      else if ( sum + add >= xs_per_bin * 0.99999999 and j < nbin-1 ){
+        // Here, we're trying to get sum+add as close to possible as xs_per_bin. 
         // Fract is equalt to the cross section (integrated over all mu) divided
         // by the number of bins. So we're going to keep on adding to this sum
         // tally and giving it more and more bin bits until it looks like its 
         // about to overflow.
         j++;
-        Float xn = getNextMuValue(fract, sum, xsVec, muVec, xsLeft, muLeft, i, invDeltaMu );
-        populateXSvector(xsVec, xn, invDeltaMu, muLeft, xsLeft, fract, i, gral, nbin, s, j, equiprobableBins);
+        Float mu = getNextMuValue(xs_per_bin, sum, xsVec, xsLeft, muLeft, muRight, i, invDeltaMu );
+        populateXSvector(xsVec, mu, invDeltaMu, muLeft, xsLeft, xs_per_bin, i, 
+                         gral, nbin, s, j, equiprobableBins);
         sum = 0.0;
 
-        if (muLeft < muVec[i-1] ){ continue; }
+        if (muLeft < muRight ){ continue; }
         else {  //std::cout << " --- 250 ---  "<< std::endl;
-          muLeft = muVec[i-1];
+          muLeft = muRight;
           xsLeft = xsVec[i-1];
           --i;
+          muRight = muVec[i-1];
           if ( i < 1 ){ return s; }
           if ( i > 1 ){ break; }
         }
       }
 
       sum  += add;
-      gral += 0.5 * (xsLeft*muVec[i-1]-xsVec[i-1]*muLeft) * 
-                    (muVec[i-1]+muLeft) + 0.333333333*(xsVec[i-1]-xsLeft) * 
-                    (muVec[i-1]*muVec[i-1]+muVec[i-1]*muLeft+muLeft*muLeft);
 
-      muLeft = muVec[i-1];
+      gral += 0.5 * (xsLeft*muRight-xsVec[i-1]*muLeft) * 
+                    (muRight+muLeft) + 0.333333333*(xsVec[i-1]-xsLeft) * 
+                    (muRight*muRight+muRight*muLeft+muLeft*muLeft);
+
+      muLeft = muRight;
       xsLeft = xsVec[i-1];
       --i;
+      muRight = muVec[i-1];
 
     } while ( i == 1 );
 
--- src/calcem/calcem_util/e_ep_mu_util/test/sigl.test.cpp
+++ src/calcem/calcem_util/e_ep_mu_util/test/sigl.test.cpp
@@ -60,7 +60,8 @@ TEST_CASE( "adaptively refine between two points in muVec grid" ){
 
 TEST_CASE( "Get pdf value" ){
   GIVEN( "material constants and energies (E->E')" ){
-    double e, tev = 0.025, tolin = 5e-2*0.5, az = 0.99917, sigma_b = 4.0, sigma_b2 = 0.0, teff = 0.12;
+    double e, tev = 0.025, tolin = 5e-2*0.5, az = 0.99917, sigma_b = 4.0, 
+           sigma_b2 = 0.0, teff = 0.12;
     int lat = 1, iinc = 2, lasym = 0;
     bool equiprobableBins = true;
     std::vector<double> alphas { 1.1, 2.2, 3.3, 4.5, 5.8 },
@@ -91,111 +92,102 @@ TEST_CASE( "Get pdf value" ){
   } // GIVEN
 } // TEST CASE
 
+/*
+*/
 
 
 
 
 TEST_CASE( "sigl" ){
-
   double e = 1e-2, tev = 0.025, tolin = 5e-2, az = 0.99917, sigma_b = 4.0, 
-  sigma_b2 = 0.0, teff = 0.12;
+         sigma_b2 = 0.0, teff = 0.12;
   int lat = 1, iinc = 2, lasym = 0, nbin = 8;
-
   std::vector<double> alphas { 1.1, 2.2, 3.3, 4.5, 5.8 },
                        betas { 0.1, 0.2, 1.3, 1.4, 2.5, 2.6, 3.7 };
-
   auto sab = populate_SAB( alphas.size(), betas.size() );
 
-
+  std::vector<double> epVec, s;
+  std::vector<std::vector<double>> correctMu;
 
   GIVEN ( "equiprobable angle bins are requested" ){
 
     bool equiprobableBins = true;
-    std::vector<double> correct_s, s;
 
-  WHEN ( "2 bins requested" ){
+
+    WHEN ( "2 angular bins requested" ){
       nbin = 2;
 
-      std::vector<double> epVec;
-      std::vector<std::vector<double>> correctMu;
+      THEN( "mu bins are correctly generated for each E, E' combination" ){
+        e = 1e-2;
+        epVec = { 1e0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5 };
+        correctMu = { { 0.0000000, 0.0000000 }, {-0.1877993, 0.6040790 },
+                      { 0.2550645, 0.8945141 }, {-0.5002791, 0.4996727 },
+                      {-0.5000937, 0.4999014 }, {-0.5000301, 0.4999693 } };
+        for ( size_t i = 0; i < correctMu.size(); ++i ){
+          s = sigl(epVec[i],e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,sigma_b,
+                   sigma_b2,teff,nbin,equiprobableBins);
+          REQUIRE(ranges::equal(correctMu[i], s, equal));
+        }
 
-      e = 1e-2;
-      epVec = { 1e0, 1e-1, 1e-2, 1e-3, 1e-4, 1e-5 };
-      correctMu = { { 0.0000000, 0.0000000 }, {-0.1877993, 0.6040790 },
-                    { 0.2550645, 0.8945141 }, {-0.5002791, 0.4996727 },
-                    {-0.5000937, 0.4999014 }, {-0.5000301, 0.4999693 } };
-      for ( size_t i = 0; i < correctMu.size(); ++i ){
-        s = sigl(epVec[i],e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,sigma_b,
-                 sigma_b2,teff,nbin,equiprobableBins);
-        REQUIRE(ranges::equal(correctMu[i], s, equal));
-      }
-
-
-      e = 1.0;
-      epVec = { 1e-2, 1e-1, 5e-1, 1.0, 2.0 };
-      correctMu = { {-0.458477, 0.527666}, {-0.289542, 0.581023}, 
-                    { 0.300582, 0.783071}, { 0.914921, 0.999029}, { 0, 0 } };
-      for ( size_t i = 0; i < correctMu.size(); ++i ){
-        s = sigl(epVec[i],e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,sigma_b,
-                 sigma_b2,teff,nbin,equiprobableBins);
-        REQUIRE(ranges::equal(correctMu[i], s, equal));
-      }
-
-  } // WHEN
+        e = 1.0;
+        epVec = { 1e-2, 1e-1, 5e-1, 1.0, 2.0 };
+        correctMu = { {-0.458477, 0.527666}, {-0.289542, 0.581023}, 
+                      { 0.300582, 0.783071}, { 0.914921, 0.999029}, { 0, 0 } };
+        for ( size_t i = 0; i < correctMu.size(); ++i ){
+          s = sigl(epVec[i],e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,sigma_b,
+                   sigma_b2,teff,nbin,equiprobableBins);
+          REQUIRE(ranges::equal(correctMu[i], s, equal));
+        }
+      } // THEN
+    } // WHEN
 
 
-
-  WHEN( "8 bins are requested" ){ 
+    WHEN( "8 bins are requested" ){ 
       
-    nbin = 8;
-    std::vector<std::vector<double>> correctMu;
-    std::vector<double> epVec;
+      nbin = 8;
 
-    e = 1e-2;
+      THEN( "mu bins are correctly generated for each E, E' combination" ){
+        epVec = { 1e-1, 1e-2, 1e-3, 1e-4, 1e-5 };
 
-    correctMu= {
-    {-0.486476,-0.285778,-0.088254, 0.109342,0.307043,0.504771,0.702703,0.901768},
-    {-0.144795, 0.165989, 0.400513, 0.598550,0.756077,0.874638,0.954019,0.993320},
-    {-0.875034,-0.625260,-0.375352,-0.125469,0.124476,0.374511,0.624475,0.875226},
-    {-0.875008,-0.625091,-0.375119,-0.125156,0.124832,0.374848,0.624852,0.875073},
-    {-0.875002,-0.625029,-0.375038,-0.125050,0.124946,0.374952,0.624954,0.875023} };
+        e = 1e-2;
+        correctMu= {
+          {-4.86476,-2.85778,-0.88254, 1.09342,3.07043,5.04771,7.02703,9.01768},
+          {-1.44795, 1.65989, 4.00513, 5.98550,7.56077,8.74638,9.54019,9.93320},
+          {-8.75034,-6.25260,-3.75352,-1.25469,1.24476,3.74511,6.24475,8.75226},
+          {-8.75008,-6.25091,-3.75119,-1.25156,1.24832,3.74848,6.24852,8.75073},
+          {-8.75002,-6.25029,-3.75038,-1.25050,1.24946,3.74952,6.24954,8.75023} };
+        for ( auto& x : correctMu ){ for (auto& y : x){ y *= 0.1; } }
     
-    epVec = { 1e-1, 1e-2, 1e-3, 1e-4, 1e-5 };
-    for ( size_t i = 0; i < correctMu.size(); ++i ){
-      s = sigl(epVec[i],e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,
-               sigma_b,sigma_b2,teff,nbin,equiprobableBins);
-      REQUIRE(ranges::equal(correctMu[i], s, equal));
-    }
+        for ( size_t i = 0; i < correctMu.size(); ++i ){
+          s = sigl(epVec[i],e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,
+                   sigma_b,sigma_b2,teff,nbin,equiprobableBins);
+          REQUIRE(ranges::equal(correctMu[i], s, equal));
+        }
 
+        e = 1e-1;
+        correctMu = {
+          { 1.64498, 8.43460, 8.91257, 9.28531,9.57995,9.78053,9.91724,9.98927},
+          {-4.86476,-2.85778,-0.88255, 1.09342,3.07043,5.04771,7.02703,9.01768},
+          {-8.62936,-5.93610,-3.31200,-0.75487,1.73851,4.17021,6.54333,8.85978},
+          {-8.71318,-6.15340,-3.61387,-1.09464,1.40475,3.88454,6.34476,8.78524},
+          {-8.73847,-6.21967,-3.70717,-1.20097,1.29896,3.79267,6.28014,8.76137} };
+        for ( auto& x : correctMu ){ for (auto& y : x){ y *= 0.1; } }
+        for ( size_t i = 0; i < correctMu.size(); ++i ){
+          s = sigl(epVec[i],e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,
+                   sigma_b,sigma_b2,teff,nbin,equiprobableBins);
+          REQUIRE(ranges::equal(correctMu[i], s, equal));
+        }
 
-    e = 1e-1;
-    correctMu = {
-    { 0.164498, 0.843460, 0.891257, 0.928531,0.957995,0.978053,0.991724,0.998927},
-    {-0.486476,-0.285778,-0.088255, 0.109342,0.307043,0.504771,0.702703,0.901768},
-    {-0.862936,-0.593610,-0.331200,-0.075487,0.173851,0.417021,0.654333,0.885978},
-    {-0.871318,-0.615340,-0.361387,-0.109464,0.140475,0.388454,0.634476,0.878524},
-    {-0.873847,-0.621967,-0.370717,-0.120097,0.129896,0.379267,0.628014,0.876137} };
-    epVec = { 1e-1,1e-2,1e-3,1e-4,1e-5 };
-    for ( size_t i = 0; i < correctMu.size(); ++i ){
-      s = sigl(epVec[i],e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,
-               sigma_b,sigma_b2,teff,nbin,equiprobableBins);
-      REQUIRE(ranges::equal(correctMu[i], s, equal));
-    }
-
-
-
-
+      } // THEN
     } // WHEN
   } // GIVEN
 
-  GIVEN( "legendre expansion is requested (in fortran, nlin (nL) is a positive number)" ){
+  GIVEN( "legendre expansion is requested" ){
     bool equiprobableBins = false;
+
     WHEN( "8 legendre order requested" ){
 
       nbin = 8;
-      std::vector<double> correct_s, s, epVec;
-      std::vector<std::vector<double>> correctMu;
-
       e = 1e-2;
       correctMu = {
       { 0.208139,  -0.126102,  0.031542, 9.98544E-3,-5.09514E-2, 3.52565E-3,
@@ -220,49 +212,35 @@ TEST_CASE( "sigl" ){
     WHEN( "19 legendre values requested" ){
       nbin = 19;
 
-      std::vector<double> correct_s, s;
+      THEN( "mu bins are correctly generated for each E, E' combination" ){
+        e = 1e-1;
 
-      e = 1e-1;
+        epVec = { 1e-5, 1e-4, 1e-3, 1e-2 };
+        correctMu = { 
+          { 3.335765E-3, -1.410863E-3, -1.034441E-4, -4.838296E-3, -2.597481E-4, 
+           -9.878173E-3, -2.526577E-4, -1.591476E-2, -1.227488E-4, -2.186337E-2,  
+            3.718295E-5, -2.551224E-2,  2.313991E-4, -2.394433E-2,  1.756766E-4, 
+           -1.474081E-2, -2.621953E-4,  2.722674E-3, -7.008888E-4 },
+          { 1.055247E-2, -1.296760E-3,  7.265585E-5, -4.604497E-3,  1.978492E-4, 
+           -9.513480E-3,  3.478084E-4, -1.560284E-2,  4.472094E-4, -2.167024E-2,  
+            3.534792E-4, -2.556965E-2, -1.213931E-4, -2.433501E-2, -1.099623E-3, 
+           -1.519392E-2, -2.405445E-3,  2.361784E-3, -3.314829E-3 },      
+          { 3.349379E-2, -4.895313E-4,  2.309690E-4, -4.638085E-3,  6.262386E-4, 
+           -9.566337E-3,  1.101285E-3, -1.564627E-2,  1.412325E-3, -2.163193E-2,  
+            1.101700E-3, -2.537224E-2, -4.212329E-4, -2.381367E-2, -3.527349E-3, 
+           -1.439508E-2, -7.610755E-3,  3.004622E-3, -1.032966E-2 },
+          { 0.2081398,  -0.1220610,    0.035420004,  0.01748036,  -0.03637126, 
+            0.01787906, -2.426432E-3, -0.02046329,   7.356535E-3, -0.01159684, 
+           -0.01680382, -1.432312E-3, -0.01757694,  -0.01579611,  -6.828400E-3, 
+           -0.01853602, -0.01128188,  -4.997656E-3, -0.01109509 } };
 
-      std::vector<double> epVec { 1e-5, 1e-4, 1e-3, 1e-2 };
-
-      double ep = 1e-2;
-      s = sigl(ep,e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,sigma_b,sigma_b2,teff,nbin,equiprobableBins);
-      correct_s = { 0.2081398, -0.1220610,  0.035420004, 0.01748036,-0.03637126, 
-      0.01787906, -2.426432E-3,-0.02046329, 7.356535E-3,-0.01159684,-0.01680382, 
-     -1.432312E-3,-0.01757694, -0.01579611,-6.828400E-3,-0.01853602,-0.01128188, 
-     -4.997656E-3,-.01109509 };
-      REQUIRE(ranges::equal(correct_s, s, equal));
-
-      ep = 1e-3;
-      correct_s = {  3.349379E-2, -4.895313E-4,  2.309690E-4, -4.638085E-3, 
-       6.262386E-4, -9.566337E-3,  1.101285E-3, -1.564627E-2,  1.412325E-3,
-      -2.163193E-2,  1.101700E-3, -2.537224E-2, -4.212329E-4, -2.381367E-2,
-      -3.527349E-3, -1.439508E-2, -7.610755E-3,  3.004622E-3, -1.032966E-2 };
-      s = sigl(ep,e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,sigma_b,sigma_b2,teff,nbin,equiprobableBins);
-      REQUIRE(ranges::equal(correct_s, s, equal));
-
-      ep = 1e-4;
-      s = sigl(ep,e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,sigma_b,sigma_b2,teff,nbin,equiprobableBins);
-      correct_s = {  1.055247E-2, -1.296760E-3,  7.265585E-5, -4.604497E-3, 
-       1.978492E-4, -9.513480E-3,  3.478084E-4, -1.560284E-2,  4.472094E-4,
-      -2.167024E-2,  3.534792E-4, -2.556965E-2, -1.213931E-4, -2.433501E-2,
-      -1.099623E-3, -1.519392E-2, -2.405445E-3,  2.361784E-3, -3.314829E-3 };      
-      REQUIRE(ranges::equal(correct_s, s, equal));
-
-
-      ep = 1e-5;
-      s = sigl(ep,e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,sigma_b,sigma_b2,teff,nbin,equiprobableBins);
-      correct_s = {  3.335765E-3, -1.410863E-3, -1.034441E-4, -4.838296E-3, 
-      -2.597481E-4, -9.878173E-3, -2.526577E-4, -1.591476E-2, -1.227488E-4, 
-      -2.186337E-2,  3.718295E-5, -2.551224E-2,  2.313991E-4, -2.394433E-2, 
-      1.7567663E-4, -1.474081E-2, -2.621953E-4,  2.722674E-3, -7.008888E-4 };
-      REQUIRE(ranges::equal(correct_s, s, equal));
-
-
+        for ( size_t i = 0; i < correctMu.size(); ++i ){
+          s = sigl(epVec[i],e,tev,tolin,lat,iinc,alphas,betas,sab,az,lasym,
+                   sigma_b,sigma_b2,teff,nbin,equiprobableBins);
+          REQUIRE(ranges::equal(correctMu[i], s, equal));
+        }
+      } // THEN 
     } // WHEN 
-
   } // GIVEN 
-
 }
 
