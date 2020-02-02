9b5b9ed8ebfee0f5cdb316a89f0be9c6ccc932fd
Amelia Trainer <ameliajo@mit.edu>
2020-02-02 17:28:53 -0500

    cleaned up some E E' mu stuff should add more test cases later too

--- src/calcem/calcem_util/e_ep_mu_util/e_ep_mu.h
+++ src/calcem/calcem_util/e_ep_mu_util/e_ep_mu.h
@@ -21,14 +21,10 @@ bool needMidpoint(const Range& x, const Range& y, const Float& xm, //const int& 
   if ( not ( i == imax or quickTest < 5e-7 or xm <= x[i-1] or xm >= x[i-2] )){ 
     Float uu  = 0, uum = 0;
 
-
-    //std::cout << y[0*imax+0] <<  "   " << y[0*imax+1] << "    " << y[0*imax+2] << std::endl;
-    //std::cout << y[0*imax-1] <<  "   " << y[0*imax] << "    " << y[0*imax+1] << std::endl;
     for ( int k = 0; k < nl; ++k ){
       ym = ( x[i-2] == x[i-1] ) ? 
         y[k*imax+i-1] :
         y[k*imax+i-1] + (xm-x[i-1])*(y[k*imax+i-2]-y[k*imax+i-1])/(x[i-2]-x[i-1]);
-      //std::cout << i << "   " << x[i-1] << "   " << y[k*imax+i-1] << "    " << x[i-2] << "    " << y[k*imax+i-2] << "     " << xm <<  "   " << ym << std::endl;
 
       if ( k > 0 ){ uu  += s[k-1]; uum += ym;  }
 
@@ -90,10 +86,6 @@ template <typename Range, typename Float>
 auto insertPoint(int& i, Range& x, Range& y, const Range& s, const Float& xm, int nl, const Float& pdf ) {
   int imax = x.size();
   //std::cout << " --- 410 --- " << std::endl;
-    //std::cout << y[0*imax+1] << "   " << y[1*imax+1] << "    " << y[2*imax+1] << std::endl;
-  //std::cout <<y[0*imax+0] <<  "   " << y[0*imax+1] <<  "   " << y[0*imax+2] << std::endl;
-  //std::cout <<y[0*imax+3] <<  "   " << y[0*imax+4] <<  "   " << y[0*imax+5] << std::endl;
-  //std::cout << std::endl;
 
   i += 1;
   x[i-1] = x[i-2];
@@ -106,8 +98,6 @@ auto insertPoint(int& i, Range& x, Range& y, const Range& s, const Float& xm, in
     y[il*imax+i-1] = y[il*imax+i-2];
     y[il*imax+i-2] = s[il-1];
   }
-  //std::cout <<y[0*imax+0] <<  "   " << y[0*imax+1] <<  "   " << y[0*imax+2] << std::endl;
-  //std::cout <<y[0*imax+3] <<  "   " << y[0*imax+4] <<  "   " << y[0*imax+5] << std::endl;
 }
 
 
@@ -140,51 +130,25 @@ auto getMoments(Float& ulast, Float& u2last, Float& u3last, const Range& y, cons
 template <typename Range, typename Float>
 auto do_330( const Float& enow, Range& x, Range& y, int& i, int& j, const Float& tev, const Float& tol, const int lat, const int iinc, const int lasym, const Range& alphas, const Range& betas, const Range& sab, const Float& az, const Float& sigma_b, const Float& sigma_b2, const Float& teff, const int nnl, const int nl, int& jbeta, Range& scr, Range& xsi, int& ie, Float& xlast, Float& ylast ){//, int& counter ){
   int imax = x.size();
-  while (true){ 
-    //std::cout << " --- 330 --- " << i << "     " << j << "    " << jbeta << std::endl;
+  while (true){  // 330
 
     if ( i < imax ){
       Float xm = 0.5*(x[i-2]+x[i-1]); 
-      //std::cout << "XM " << x[i-2] << "   " << x[i-1] << "    " << xm  << std::endl;
       xm = sigfig(xm,8,0);
-      //std::cout << i <<  "   " << x[i-2] << "   " << x[i-1] << std::endl;
       Range s(abs(nnl)-1,0.0);
-      //std::cout << enow << "   " << xm << std::endl;
       Float pdf = sigl(xm,enow,tev,tol,lat,iinc,alphas,betas,sab,az,lasym,sigma_b,sigma_b2,teff,s,true);
-    std::cout << "YT   " << xm << "    " << pdf << "    " << s[0] << "     " << s[1] << "     " << s[2] << std::endl;
   
-
-    //if ( xm > 9.6e-2 ){ 
-    //    double ulast = 0,u2last = 0,u3last = 0;
-      //return std::make_tuple(ulast,u2last,u3last);  // HERE
-   // }
-
-
-    //std::cout << y[0*imax+0] << "   " << y[0*imax+1] << "    " << y[0*imax+2] << std::endl;
-    //std::cout << y[0*imax+3] << "   " << y[0*imax+4] << "    " << y[0*imax+5] << std::endl;
       if ( needMidpoint(x, y, xm, i, nl, s, tol, pdf) == true ){ 
-          //std::cout << " ---- 410 --- " << std::endl;
         insertPoint(i, x, y, s, xm, nl, pdf );
-        //std::cout << x[0] << "    " << x[1] << "    " << x[2] << "   " << x[3] << "     " << x[4] << std::endl;
         continue; 
       }
     }
 
 
 
-
     //std::cout << " --- 360 --- " << std::endl;
-    //std::cout << y[0*imax+0] << "    " << y[0*imax+1] << "   " << y[0*imax+2] << "    " << y[0*imax+3] << std::endl;
-    //std::cout << y[1*imax+0] << "    " << y[1*imax+1] << "   " << y[1*imax+2] << "    " << y[1*imax+3] << std::endl;
-
-    //std::cout << x[0] << "    " << x[1] << "    " << x[2] << "   " << x[3] << "     " << x[4] << std::endl;
-
-    //std::cout << i << "     " << j << "   " << x[i-1] << std::endl;
     ++j;
     if (j > 1) { xsi[ie] += (x[i-1]-xlast)*(y[0*imax+i-1]+ylast)*0.5; }
-    //std::cout << xsi[ie] << std::endl;
-
-
     if ( j == 3 and xsi[ie] < 5e-7 ){ j = 2; }
 
 
@@ -199,35 +163,22 @@ auto do_330( const Float& enow, Range& x, Range& y, int& i, int& j, const Float&
 
 
     //std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << jscr-1 << "     " << scr[jscr-1] << std::endl;
-    std::cout << jscr   << "     " << scr[jscr] << std::endl;
+    //std::cout << jscr-1 << "     " << scr[jscr-1] << std::endl;
+    //std::cout << jscr   << "     " << scr[jscr] << std::endl;
 
     for ( int il = 1; il < nl; ++il ){
-        //std::cout << y[il*imax+i-1] << std::endl;
       scr[il+jscr] = sigfig(y[il*imax+i-1],9,0);
       if (scr[il+jscr] > 1.0){ y[il*imax+i-1] = 1.0; }
       if (scr[il+jscr] <-1.0){ y[il*imax+i-1] =-1.0; }
-      std::cout << jscr+il << "     " <<  scr[jscr+il] << std::endl;
+      //std::cout << jscr+il << "     " <<  scr[jscr+il] << std::endl;
     }
-    //std::cout << std::endl;
-
 
     Float ulast, u2last, u3last;
     getMoments(ulast, u2last, u3last, y, i, x.size(), nl);
 
 
-    //std::cout << i << std::endl;
-    //if ( i == 18 ){
-    //return std::make_tuple(ulast,u2last,u3last);  // HERE
-   // }
-
     --i;
     if ( i >= 2 ){ continue; } // go to 330
-    //std::cout << y[0*imax+0] << "   " << y[0*imax+1] << "    " << y[0*imax+2] << std::endl;
-    //std::cout << y[0*imax+3] << "   " << y[0*imax+4] << "    " << y[0*imax+5] << std::endl;
-
-
 
     ++jbeta;
     return std::make_tuple(ulast,u2last,u3last);  // go to 311 or 430
@@ -279,8 +230,6 @@ auto do_330_extra( const Float& enow, int& j, const Float& tev, const Float& tol
     //std::cout << y[1*imax+0] << "   " << y[1*imax+1] << "   " << y[1*imax+2] << "    " << y[1*imax+3] << std::endl;
     //std::cout << y[2*imax+0] << "   " << y[2*imax+1] << "   " << y[2*imax+2] << "    " << y[2*imax+3] << std::endl;
     //std::cout << y[3*imax+0] << "   " << y[3*imax+1] << "   " << y[3*imax+2] << "    " << y[3*imax+3] << std::endl;
-    //std::cout << y[4*imax+0] << "   " << y[4*imax+1] << "   " << y[4*imax+2] << "    " << y[4*imax+3] << std::endl;
-    //std::cout << y[5*imax+0] << "   " << y[5*imax+1] << "   " << y[5*imax+2] << "    " << y[5*imax+3] << std::endl;
     //std::cout << std::endl;
 
 
@@ -288,21 +237,8 @@ auto do_330_extra( const Float& enow, int& j, const Float& tev, const Float& tol
   
     do_330(enow,x,y,i,j,tev,tol,lat,iinc,lasym,alphas,betas,sab,az,sigma_b,sigma_b2,teff,nnl,nl,jbeta,scr,xsi,ie,xlast,ylast);
 
-    //auto ulast = std::get<0>(out);
-    //if (ulast == 0){ return; }
-    //std::cout << " after 330 " << jbeta << "     " << ep << std::endl;
-    //std::cout << y[0*imax+0] << "   " << y[0*imax+1] << "   " << y[0*imax+2] << std::endl;
-    //std::cout << y[1*imax+0] << "   " << y[1*imax+1] << "   " << y[1*imax+2] << std::endl;
-    //std::cout << y[2*imax+0] << "   " << y[2*imax+1] << "   " << y[2*imax+2] << std::endl;
-    //std::cout << std::endl;
-
-
-
-
-    //return; // HERE
 
     if (jbeta <= int(betas.size())){ 
-        //std::cout << " go to 311 " << std::endl;
       continue; // go to 311
     }
     else { // go to 430
@@ -315,8 +251,6 @@ auto do_330_extra( const Float& enow, int& j, const Float& tev, const Float& tol
       double sum = 0.0;
       for ( size_t k = 2; k < scr.size(); ++k ){
         if ( (k-1)%6 == 0 ){
-          //std::cout << scr[k-6] << "    " << scr[k] << std::endl;
-            //std::cout << k << "   " << std::endl;
           sum += (scr[k]+scr[k-6])*0.5*(scr[k-1]-scr[k-7]);
         }
       }
@@ -325,10 +259,8 @@ auto do_330_extra( const Float& enow, int& j, const Float& tev, const Float& tol
         if ( (k-1)%6 == 0 ){
           scr[k] = scr[k]/sum;
         }
-       // std::cout << scr[k] << std::endl;
       }
 
-      //std::cout << " go to 430 " << std::endl; 
       return;
     }
 
